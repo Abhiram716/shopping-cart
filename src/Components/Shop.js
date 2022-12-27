0a4df228696ad4b6da5fa4eb0eb852b2575f5b79
Abhiram Kantipudi <78171758+Abhiram716@users.noreply.github.com>
2022-12-27 11:14:30 +0530

    Update Shop.js

--- src/Components/Shop.js
+++ src/Components/Shop.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import {Body}  from "./Body";
+import Body  from "./Body";
 
-export const Shop = ({ handleClick }) => {
+export default const Shop = ({ handleClick }) => {
     const [items, setItems] = useState([]);
 
     const fetchData = async () => {
